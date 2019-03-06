a781d9cddc83e051e0f2c0b664e7670b0f482fe9
Gilles Lenfant <gilles.lenfant@gmail.com>
2019-03-06 10:10:55 +0100

    INvoice class (WIP)

--- src/ubllib/documents/invoice.py
+++ src/ubllib/documents/invoice.py
@@ -8,9 +8,14 @@ from lxml import etree
 from ..base import BaseDocument
 from ..namespaces import INVOICE_NS, clark_tag
 
+
 class Invoice(BaseDocument):
     namespace = INVOICE_NS
     tag = 'Invoice'
+
+    def __init__(self):
+        """
+        """
 
     @classmethod
     def from_element(cls, tree: etree._ElementTree):
